1df97ada1f6d508d0b6ac63844d5e896add08ba6
Mark <pi@none>
2019-12-15 22:50:14 +0000

    Add start of digit read

---
+++ Digit_Read/digit_read.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 22:31:46 2019
+
+@author: pi
+"""
+
+class Digit:
+    pass
---
+++ Digit_Read/test_digit_read.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 22:33:14 2019
+
+@author: pi
+"""
+
+from unittest import TestCase
+from digit_read import Digit
+
+class TestDigitRead(TestCase):
+    def test_practice(self):
+        comp = Digit()
+        self.assertEqual("m", "p")
