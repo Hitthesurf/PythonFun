cff7ca69471a719584a743b6b7584939ae94ba16
Mark <pi@none>
2020-02-04 18:49:58 +0000

    Add more tests

--- GameOfLife/GameOfLife.py
+++ GameOfLife/GameOfLife.py
@@ -12,4 +12,15 @@ class GameOfLifeGrid:
         self.current_grid = []
     
     def get_neighbour_count(self, down, right):
-        return 3
+        blocks = [[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]]
+        neighbour_count = 0
+        for direction in blocks:
+            temp_right = right + direction[0]
+            temp_down = down + direction[1]
+            value = self.get_value(temp_down, temp_right)
+            if value:
+                neighbour_count += 1
+        return neighbour_count
+            
+    def get_value(self, down, right):
+        return self.current_grid[down-1][right-1]
--- GameOfLife/test_GameOfLife.py
+++ GameOfLife/test_GameOfLife.py
@@ -9,16 +9,47 @@ Created on Tue Feb  4 17:33:26 2020
 from unittest import TestCase
 from GameOfLife import GameOfLifeGrid
 
-class TestGameOfLife(TestCase):
-    def test_get_neighbour_count_is_correct_when_point_not_on_boundary(self):
+class TestGameOfLifeGridCount(TestCase):
+    def before_each_test(self):
         grid = GameOfLifeGrid()
         #grid.grid_size = [6,6]
         input_grid = [[0,1,1,0,0],
                       [1,1,0,0,1],
                       [0,0,1,0,0]]
-        grid.curent_grid = input_grid
+        grid.current_grid = input_grid
+        return grid
+        
+    def test_get_neighbour_count_is_correct_when_point_not_on_boundary_1(self):
+        grid = self.before_each_test()
         in_right = 3
+        in_down = 2
+        expected = 4
+        actual = grid.get_neighbour_count(in_down, in_right)
+        self.assertEqual(expected, actual)
+        
+    def test_get_neighbour_count_is_correct_when_point_not_on_boundary_2(self):
+        grid = self.before_each_test()
+        in_right = 4
         in_down = 2
         expected = 3
         actual = grid.get_neighbour_count(in_down, in_right)
         self.assertEqual(expected, actual)
+    
+    def test_get_value_gets_the_correct_value_for_position_in_range_1(self):
+        grid = self.before_each_test()
+        in_right = 3
+        in_down = 2
+        expected = 0
+        actual = grid.get_value(in_down, in_right)
+        self.assertEqual(expected, actual)
+        
+    def test_get_value_gets_the_correct_value_for_position_in_range_2(self):
+        grid = self.before_each_test()
+        in_right = 3
+        in_down = 1
+        expected = 1
+        actual = grid.get_value(in_down, in_right)
+        self.assertEqual(expected, actual)
+    
+    
+    
